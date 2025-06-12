b72edaeb6d77ba2129ad8fc5b4ecc6d8faf78465
Mahesh-chef <mahesh.vardolu789@gmail.com>
2025-06-11 22:57:20 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/blue-resume-forge">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
